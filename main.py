07f29ac357ecc5eb6d06b422d0c3dcc35b4a0313
Andrew Yaremenko <andreiyaremenko9@gmail.com>
2024-03-31 17:00:55 +0300

    changed gitignore

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import numpy as np
 import lib
 
 
-def solve_via_QUBO(A: np.ndarray, b: np.ndarray, option: str = 'gekko', tol: float = 0.001) -> np.ndarray:
+def solve_via_QUBO(A: np.ndarray, b: np.ndarray, lb: float = -10, ub: float = 10, option: str = 'gekko', tol: float = 0.1) -> np.ndarray:
     assert A.ndim == 2
     assert b.ndim == 1
     assert np.size(A, axis=1) == np.size(b)
     n = b.size
-    lb = -100 * np.ones(n, dtype='float')
-    ub = 100 * np.ones(n, dtype='float')
+    lb = lb * np.ones(n)
+    ub = ub * np.ones(n)
     while ub[0] - lb[0] > tol:
         k = (ub - lb) / 2
         d = lb + (ub - lb) / 4
@@ -23,6 +23,6 @@ def solve_via_QUBO(A: np.ndarray, b: np.ndarray, option: str = 'gekko', tol: flo
 if __name__ == '__main__':
     A = np.array([[-1, 1], [1, 0]])
     b = np.array([2, 3])
-    # print(solve_exact(A, b))
-    # print(solve_slsqp(A, b, verbose=True))
+    print(lib.solve_exact(A, b))
+    print(lib.solve_slsqp(A, b))
     print(solve_via_QUBO(A, b))
