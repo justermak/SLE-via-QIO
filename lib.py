31e06f4ef9fd4821632dc3435e47a7ef7acd439d
justermak <andreiyaremenko9@gmail.com>
2024-04-07 22:21:17 +0300

    aboba

--- lib.py
+++ lib.py
@@ -3,36 +3,66 @@ import math
 import numpy as np
 from scipy.optimize import minimize as scp_minimize
 from gekko import GEKKO
-from dwave_qbsolv import QBSolv
+from dwave.samplers import SimulatedAnnealingSampler
 import itertools
 
-
-MAX_BORDER = 100
-EPS = 0.1
+max_border = 1e5
+eps = 1e-5
+base_prec = 3
 
 
 def to_QP(A: np.ndarray, b: np.ndarray) -> (np.ndarray, np.ndarray):
     assert A.ndim == 2
     assert b.ndim == 1
-    assert np.size(A, axis=1) == np.size(b)
+    n, m = A.shape
+    assert b.size == n
     return A.T @ A, -2 * b.T @ A
 
 
-def substitution_kx_plus_d(A: np.ndarray, b: np.ndarray, k: np.ndarray, d: np.ndarray) -> (
+def substitute_kx_plus_d(A: np.ndarray, b: np.ndarray, k: np.ndarray, d: np.ndarray) -> (
         np.ndarray, np.ndarray):
     assert A.ndim == 2
     assert b.ndim == 1
-    assert np.size(A, axis=1) == np.size(b)
-    assert k.size == np.size(A, axis=1)
-    assert d.size == np.size(A, axis=1)
+    n, m = A.shape
+    assert b.size == n
+    assert k.size == m
+    assert d.size == m
+    AA = A * k
+    bb = b - A @ d
+    return AA, bb
+
+
+def substitute_linear_form(A: np.ndarray, b: np.ndarray, c: np.ndarray) -> (np.ndarray, np.ndarray):
+    assert A.ndim == 2
+    assert b.ndim == 1
+    n, m = A.shape
+    assert b.size == n
+    assert c.shape[0] == m
+    # x = c[0] + c[1] * x_1 + ... + c[n-1] * x_{n-1}
     n = np.size(A, axis=1)
-    return A * k, b - A @ d
+    k = np.size(c, axis=1) - 1
+    const, coef = c[:, 0], c[:, 1:]
+    AA = np.repeat(A, k, axis=1) * coef.flatten()
+    bb = b - A @ const
+    return AA, bb
+
+
+def extract_solution_from_substitution(res: np.ndarray, c: np.ndarray) -> np.ndarray:
+    assert res.ndim == 1
+    assert c.ndim == 2
+    n = np.size(c, axis=0)
+    k = np.size(c, axis=1) - 1
+    assert res.size == n * k
+    const, coef = c[:, 0], c[:, 1:]
+    res.reshape((n, k))
+    vals = const + (coef * res.reshape(n, k)).sum(axis=1)
+    return vals
 
 
 def QUBO_bruteforce(A: np.ndarray) -> np.ndarray:
     assert A.ndim == 2
-    assert A.shape == A.shape[::-1]
-    n = np.size(A, axis=1)
+    n, m = A.shape
+    assert n == m
     minimum = np.inf
     argmin = None
     for x in itertools.product(range(2), repeat=n):
@@ -46,24 +76,25 @@ def QUBO_bruteforce(A: np.ndarray) -> np.ndarray:
 
 def QUBO_gekko(A: np.ndarray) -> np.ndarray:
     assert A.ndim == 2
-    assert A.shape == A.shape[::-1]
-    n = np.size(A, axis=1)
-    m = GEKKO(remote=False)
-    x = m.Array(m.Var, (n,), value=0, lb=0, ub=1, integer=True)
-    m.Obj(x.T @ A @ x)
+    n, m = A.shape
+    assert n == m
+    solver = GEKKO(remote=False)
+    x = solver.Array(solver.Var, (n,), lb=0, ub=1, integer=True)
+    solver.Obj(x.T @ A @ x)
     try:
-        m.solve(disp=False)
+        solver.solve(disp=False)
     except Exception:
-        print(f"gekko unable to solve QUBO")
+        print(f"gekko can't solve QUBO")
         return np.zeros(n)
-    return np.vectorize(lambda x: x[0])(x)
+    return np.vectorize(lambda v: v[0])(x)
 
 
 def QUBO_dwave(A: np.ndarray) -> np.ndarray:
     assert A.ndim == 2
-    assert A.shape == A.shape[::-1]
-    n = np.size(A, axis=1)
-    response = QBSolv().sample_qubo({(i, j): A[i][j] for (i, j) in itertools.product(range(n), repeat=2)})
+    n, m = A.shape
+    assert n == m
+    sampler = SimulatedAnnealingSampler()
+    response = sampler.sample_qubo({(i, j): A[i][j] for (i, j) in itertools.product(range(n), repeat=2)})
     samples = response.samples()
     return np.array([samples[0][i] for i in range(n)])
 
@@ -80,58 +111,117 @@ def solve_QUBO(A: np.ndarray, option: str) -> np.ndarray:
             raise NotImplemented()
 
 
-def solve_exact(A: np.ndarray, b: np.ndarray) -> np.ndarray:
+def solve_np(A: np.ndarray, b: np.ndarray) -> np.ndarray:
     assert A.ndim == 2
     assert b.ndim == 1
-    assert np.size(A, axis=1) == np.size(b)
+    n, m = A.shape
+    assert b.size == n
+    assert n == m, "A should be square matrix"
     return np.linalg.solve(A, b)
 
 
 def solve_slsqp(A: np.ndarray, b: np.ndarray, tol: float = 0.01, verbose: bool = False) -> np.ndarray:
     assert A.ndim == 2
     assert b.ndim == 1
-    n = np.size(A, axis=1)
+    n, m = A.shape
     assert b.size == n
     if verbose:
-        print(f"SLSQP solving SLE with A={A}, b={b}")
+        print(f"SLSQP solving SLE with\nA={A}\nb={b}")
     AA, bb = to_QP(A, b)
-    fun = lambda x, *args: x.T @ AA @ x + bb @ x
-    callback = lambda intermediate_result: print(intermediate_result) \
-        if verbose else \
-        lambda intermediate_result: ...
-    result = scp_minimize(fun, np.zeros(n), method='SLSQP', tol=tol, callback=callback)
+
+    def fun(x, *args):
+        return (x.T @ AA + bb) @ x
+
+    def callback(xk):
+        if verbose:
+            print(f"xk={xk}")
+
+    result = scp_minimize(fun, np.zeros(m), method='SLSQP', tol=tol, callback=callback)
     if verbose:
         print(f"Exiting SLSQP")
     return result['x']
 
 
-def solve_DNC_QUBO(A: np.ndarray, b: np.ndarray, lb: np.ndarray = None, ub: np.ndarray = None, option: str = 'bruteforce',
-                         tol: float = 0.01, verbose: bool = False) -> np.ndarray:
+def solve_reference(A: np.ndarray, b: np.ndarray, option: str) -> np.ndarray:
+    match option:
+        case 'np':
+            return solve_np(A, b)
+        case 'slsqp':
+            return solve_slsqp(A, b)
+        case _:
+            raise NotImplemented()
+
+
+def solve_DNC_QUBO(A: np.ndarray, b: np.ndarray, option: str = 'bruteforce', lb: np.ndarray = None,
+                   ub: np.ndarray = None, tol: float = eps, verbose: bool = False) -> np.ndarray:
     assert A.ndim == 2
     assert b.ndim == 1
-    n = np.size(A, axis=1)
+    n, m = A.shape
     assert b.size == n
     if lb is not None:
-        assert lb.size == n
+        assert lb.size == m
     else:
-        lb = -MAX_BORDER * np.ones(n)
+        lb = -max_border * np.ones(m)
     if ub is not None:
-        assert ub.size == n
+        assert ub.size == m
     else:
-        ub = MAX_BORDER * np.ones(n)
+        ub = max_border * np.ones(m)
     if verbose:
-        print(f"Bisection QUBO solving SLE with A={A}, b={b}, lb={lb}, ub={ub}")
-    it = math.ceil(math.log2(max(ub - lb)/tol))
+        print(f"DNC QUBO solving SLE with\nA={A}\nb={b}\nlb={lb}\nub={ub}\ntol={tol}")
+    it = math.ceil(math.log2(max(ub - lb) / tol))
     for i in range(it):
         k = (ub - lb) / 2
         d = lb + (ub - lb) / 4
-        A_sub, b_sub = substitution_kx_plus_d(A, b, k, d)
+        A_sub, b_sub = substitute_kx_plus_d(A, b, k, d)
         AA, bb = to_QP(A_sub, b_sub)
-        x = solve_QUBO(AA + bb, option)
+        x = solve_QUBO(AA + np.diag(bb), option)
         ub -= k * (1 - x)
         lb += k * x
         if verbose:
-            print(f"x={x}, lb={lb}, ub={ub}")
+            print(f"iter={i}\nk={k}\nd={d}\nA_sub={A_sub}\nb_sub={b_sub}\nAA={AA}\nbb={bb}\nx={x}\nub={ub}\nlb={lb}\n")
     if verbose:
-        print(f"Exiting Bisection QUBO")
+        print(f"Exiting DNC QUBO")
     return lb
+
+
+def to_finite_precision_form(lb: np.ndarray, ub: np.ndarray, prec: int) -> (np.ndarray, np.ndarray):
+    assert lb.ndim == 1
+    assert ub.ndim == 1
+    assert lb.size == ub.size
+    assert prec > 0
+    n = lb.size
+    k = np.log2((ub - lb)).astype(int) + 1
+    c = np.array([[lb[i]] + [2 ** j for j in range(k[i] - prec, k[i])] for i in range(n)])
+    return c
+
+
+def solve_one_step_QUBO(A: np.ndarray, b: np.ndarray, option: str = 'bruteforce', lb: np.ndarray = None,
+                        ub: np.ndarray = None, prec: int = None, verbose: bool = False) -> np.ndarray:
+    assert A.ndim == 2
+    assert b.ndim == 1
+    n, m = A.shape
+    assert b.size == n
+    if lb is not None:
+        assert lb.size == m
+    else:
+        lb = -max_border * np.ones(m)
+    if ub is not None:
+        assert ub.size == m
+    else:
+        ub = max_border * np.ones(m)
+    if prec is not None:
+        assert prec > 0
+    else:
+        prec = base_prec
+    if verbose:
+        print(f"One step QUBO solving SLE with\nA={A}\nb={b}\nlb={lb}\nub={ub}\nprec={prec}")
+    c = to_finite_precision_form(lb, ub, prec)
+    A_sub, b_sub = substitute_linear_form(A, b, c)
+    AA, bb = to_QP(A_sub, b_sub)
+    x = solve_QUBO(AA + np.diag(bb), option)
+    vals = extract_solution_from_substitution(x, c)
+    if verbose:
+        print(f"c={c}\nA_sub={A_sub}\nb_sub={b_sub}\nAA={AA}\nbb={bb}\nx={x}\nvals={vals}\n")
+    if verbose:
+        print(f"Exiting one step QUBO")
+    return vals
--- test.py
+++ test.py
@@ -1,70 +1,120 @@
 import numpy as np
+from scipy.stats import ortho_group
 import itertools
+import pandas as pd
+from typing import Iterator, Tuple
 import os
 from pathlib import Path
 import matplotlib.pyplot as plt
-import pandas as pd
-
 import lib
-
 
 rng = np.random.default_rng()
 directory = Path(f"{os.path.dirname(__file__)}")
 tests_path = directory / Path("tests")
+test_groups = 4
+group_size = 10
+matrix_size = 5
 
 
-def write_eqs(A, b, filename):
-    np.savez(filename, A + b)
+def write_eqs(eqs: Iterator[Tuple[np.ndarray, np.ndarray]], filename: str) -> None:
+    As, bs = zip(*eqs)
+    np.savez(filename, *(list(As) + list(bs)))
 
 
-def read_eqs(filename):
-    d = np.load(filename)
-    x = d.values()
-    return x[:len(x)/2], x[len(x)/2:]
+def read_eqs(filename: str) -> Iterator[Tuple[np.ndarray, np.ndarray]]:
+    assert os.path.exists(filename + ".npz")
+    d = np.load(filename + ".npz")
+    x = d.values().__iter__()
+    return zip(x.__next__(), x.__next__())
 
 
-def generate_uniform(l1: float, r1: float, l2: float, r2: float, size: int, n: int):
+def generate_by_solution(res: np.ndarray, size: int, n: int):
     for i in range(n):
-        A = rng.uniform(l1, r1, (size, size))
-        b = rng.uniform(l2, r2, size)
+        A = ortho_group.rvs(size)
+        b = A @ res
         yield A, b
 
 
-def generate_all(n: int):
-    for e in generate_uniform(-5, 5, -10, 10, 10, n):
+def generate_by_eigenvalues(eigenvalues: np.ndarray, size: int, n: int):
+    for i in range(n):
+        a = ortho_group.rvs(size)
+        A = a @ np.diag(eigenvalues) @ a.T
+        res = rng.uniform(-1, 1, size)
+        b = A @ res
+        yield A, b
+
+
+def generate_all():
+    for e in generate_by_solution(np.array([1] * matrix_size), matrix_size, group_size):
+        yield e
+    for e in generate_by_solution(np.array([1000] + [1] * (matrix_size - 1)), matrix_size, group_size):
+        yield e
+    for e in generate_by_eigenvalues(np.array([1] * matrix_size), matrix_size, group_size):
+        yield e
+    for e in generate_by_eigenvalues(np.array([1000] + [1] * (matrix_size - 1)), matrix_size, group_size):
         yield e
 
 
-def solve_all(qubo_option: str, n: int) -> pd.DataFrame:
-    tests = [*generate_all(n)]
-    np.savez(tests_path, *[item for pair in tests for item in pair])
-    exact_solutions = np.array([lib.solve_exact(A, b) for A, b in tests])
-    DNC_QUBO_solutions = np.array([lib.solve_DNC_QUBO(A, b, option=qubo_option) for A, b in tests])
+def get_tests(option: str) -> Iterator[Tuple[np.ndarray, np.ndarray]]:
+    match option:
+        case 'load':
+            path = (tests_path / Path("tests"))
+            return read_eqs(path.absolute().as_posix())
+        case 'generate':
+            return generate_all()
+        case _:
+            raise Exception('Incorrect option. Should be load / generate')
+
+
+def solve_all(tests: Iterator[Tuple[np.ndarray, np.ndarray]], qubo_option: str = 'bruteforce',
+              reference_option: str = 'np', option: str = 'save') -> pd.DataFrame:
+    match option:
+        case 'save':
+            tests, write_tests = itertools.tee(tests)
+            path = (tests_path / Path("tests"))
+            if not os.path.exists(tests_path):
+                os.makedirs(tests_path)
+            write_eqs(write_tests, path.absolute().as_posix())
+        case 'discard':
+            pass
+        case _:
+            raise Exception('Incorrect option. Should be save / discard')
     df = pd.DataFrame()
-    df['exact_norms'] = np.apply_along_axis(np.linalg.norm, 1, exact_solutions)
-    df['DNC_QUBO_norms'] = np.apply_along_axis(np.linalg.norm, 1, DNC_QUBO_solutions)
-    df['difference_norms'] = np.apply_along_axis(np.linalg.norm, 1, exact_solutions - DNC_QUBO_solutions)
-    df['ratios'] = df['difference_norms'] / df['exact_norms']
-    df = df[df['DNC_QUBO_norms'] < 1e9]
+    solutions = [[lib.solve_reference(A, b, reference_option), lib.solve_DNC_QUBO(A, b, qubo_option),
+                  lib.solve_one_step_QUBO(A, b, qubo_option, lb=np.array([-2] * matrix_size),
+                                          ub=np.array([2] * matrix_size))] for A, b in tests]
+    df[['reference_solution', 'DNC_QUBO_solution', 'One_step_QUBO_solutions']] = solutions
+    df['reference_norm'] = np.vectorize(np.linalg.norm)(df['reference_solution'])
+    df['DNC_QUBO_norm'] = np.vectorize(np.linalg.norm)(df['DNC_QUBO_solution'])
+    df['One_step_QUBO_norm'] = np.vectorize(np.linalg.norm)(df['One_step_QUBO_solutions'])
+    df['DNC_diff'] = np.vectorize(np.linalg.norm)(df['reference_solution'] - df['DNC_QUBO_solution'])
+    df['One_step_diff'] = np.vectorize(np.linalg.norm)(df['reference_solution'] - df['One_step_QUBO_solutions'])
+    df['DNC_ratio'] = df['DNC_diff'] / df['reference_norm']
+    df['One_step_ratio'] = df['One_step_diff'] / df['reference_norm']
     return df
 
 
-def test_and_plot(qubo_option: str, test_size: str):
-    match test_size:
-        case 'big': n = 100
-        case 'small': n = 10
-        case _: raise Exception('Incorrect test_size. Should be big / small')
-    df = solve_all(qubo_option, n)
-    n = df.shape[0]
-    plt.scatter(range(n), df['difference_norms'], c='red')
-    plt.title("Absolute accuracy of DNC QUBO")
-    plt.xlabel("Test number")
-    plt.ylabel("Norm difference")
-    plt.savefig(directory / Path('!absolute_errors.png'))
+def plot(title: str, data: pd.DataFrame, path: Path):
+    fig, axs = plt.subplots(2, 2, figsize=(10, 10))
+    fig.tight_layout(pad=5)
+    fig.suptitle(title)
+    for i in range(test_groups):
+        axs[i // 2, i % 2].scatter(range(group_size), data[i * group_size:(i + 1) * group_size])
+        axs[i // 2, i % 2].set_title(f'Group {i}')
+    if not os.path.exists(tests_path):
+        os.makedirs(tests_path)
+    plt.savefig(tests_path / path)
     plt.clf()
-    plt.scatter(range(n), df['ratios'], c='red')
-    plt.xlabel("Test number")
-    plt.ylabel("Relative norm difference")
-    plt.title("Relative accuracy of DNC QUBO")
-    plt.savefig(directory / Path('!relative_errors.png'))
-    plt.scatter(range(n), df['difference_norms'], c='red')
+
+
+def test_and_plot(qubo_option: str, refecence_option: str, test_option: str, save_option: str):
+    tests = get_tests(test_option)
+    df = solve_all(tests, qubo_option=qubo_option, reference_option=refecence_option, option=save_option)
+
+    plot('Absolute error (||x - x*||)', df['DNC_diff'], Path('DNC_diff.png'))
+    plot('Absolute error (||x - x*||)', df['One_step_diff'], Path('One_step_diff.png'))
+    plot('Relative error (||x - x*|| / ||x*||)', df['DNC_ratio'], Path('DNC_ratio.png'))
+    plot('Relative error (||x - x*|| / ||x*||)', df['One_step_ratio'], Path('One_step_ratio.png'))
+    plot('Norm of reference solution', df['reference_norm'], Path('reference_norm.png'))
+    plot('Norm of DNC solution', df['DNC_QUBO_norm'], Path('DNC_norm.png'))
+    plot('Norm of One step solution', df['One_step_QUBO_norm'], Path('One_step_norm.png'))
