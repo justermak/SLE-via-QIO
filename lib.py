e0493c485598178b1a22936c83737158efb6b8f4
justermak <andreiyaremenko9@gmail.com>
2024-04-30 19:40:11 +0300

    new graphs

--- lib.py
+++ lib.py
@@ -1,16 +1,11 @@
 import numpy as np
-import scipy.stats as ss
 from scipy.optimize import minimize as scp_minimize
 from dwave.samplers import SimulatedAnnealingSampler
 import itertools
-
 rng = np.random.default_rng()
-base_prec = 1
-base_neighbors = 0
-base_iter = 3
+default_niter = 1
 bound = 1000
 eps = 1e-1
-sigma = 1 / 4
 
 
 def to_QP(A: np.ndarray, b: np.ndarray) -> (np.ndarray, np.ndarray):
@@ -75,27 +70,16 @@ def solve_reference(A: np.ndarray, b: np.ndarray, tol: float = 0.01, verbose: bo
 
 
 def solve_one(A: np.ndarray, b: np.ndarray, prec: int, neighbors: int, lb: np.ndarray, ub: np.ndarray, tol: float,
-              perturb: bool, verbose: bool) -> np.ndarray:
-    n = A.shape[0]
+              verbose: bool) -> np.ndarray:
     if verbose:
         print(f"Solving\nA={A}\nb={b}\n")
     it = 0
     while (ub - lb).max() > tol:
         it += 1
         lengths = (ub - lb) / 2 ** prec
-        c = []
-        for i in range(n):
-            rnd = ss.truncnorm(-1 / 2 / sigma, 1 / 2 / sigma, 1 / 2, sigma).rvs() if perturb else 1 / 2
-            mn, mx = rnd, rnd
-            const = rnd * lengths[i] + lb[i]
-            coefs = []
-            for k in range(prec):
-                rnd = ss.truncnorm(-mn / sigma, (1 - mx) / sigma, 0, sigma).rvs() if perturb else 0
-                coefs += [(rnd + 2 ** k) * lengths[i]]
-                mn = min(mn, mn + rnd)
-                mx = max(mx, mx + rnd)
-            c += [[const] + coefs]
-        c = np.array(c)
+        const = lengths / 2 + lb
+        coefs = np.array([2 ** k for k in range(prec)]).reshape(1, -1) * lengths.reshape(-1, 1)
+        c = np.hstack((const[:, None], coefs))
         AA, bb = substitute(A, b, c)
         q = solve_QUBO(AA + np.diag(bb))
         box = extract_solution_box(q, prec)
@@ -109,13 +93,13 @@ def solve_one(A: np.ndarray, b: np.ndarray, prec: int, neighbors: int, lb: np.nd
 
 
 def solve_iter(A: np.ndarray, b: np.ndarray, prec: int, neighbors: int, niter: int, lb: np.ndarray, ub: np.ndarray,
-               tol: float, perturb: bool, verbose: bool) -> np.ndarray:
+               tol: float, verbose: bool) -> np.ndarray:
     n = A.shape[0]
     res = [np.zeros(n)]
     if verbose:
         print(f"Solving iteratively\nA={A}\nb={b}\n")
     for iter in range(niter):
-        res += [solve_one(A, b, prec, neighbors, lb, ub, tol, perturb, verbose)]
+        res += [solve_one(A, b, prec, neighbors, lb, ub, tol, verbose)]
         y = eval(A, b, res[-1])
         if verbose:
             print(f"iter={iter}\ny={y}\nres={res[-1]}\n")
@@ -126,12 +110,11 @@ def solve_iter(A: np.ndarray, b: np.ndarray, prec: int, neighbors: int, niter: i
     return np.array(res).sum(axis=0)
 
 
-def solve(A: np.ndarray, b: np.ndarray, prec: int = base_prec, neighbors: int = base_neighbors, niter: int = base_iter,
-          lb: np.ndarray = None, ub: np.ndarray = None, tol: float = eps, perturb: bool = False,
-          verbose: bool = False) -> np.ndarray:
+def solve(A: np.ndarray, b: np.ndarray, prec: int, neighbors: int, niter: int = default_niter,
+          lb: np.ndarray = None, ub: np.ndarray = None, tol: float = eps, verbose: bool = False) -> np.ndarray:
     n = A.shape[0]
     if lb is None:
         lb = -bound * np.ones(n)
     if ub is None:
         ub = bound * np.ones(n)
-    return solve_iter(A, b, prec, neighbors, niter, lb, ub, tol, perturb, verbose)
+    return solve_iter(A, b, prec, neighbors, niter, lb, ub, tol, verbose)
--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 import numpy as np
 from scipy.stats import ortho_group
-import pandas as pd
 from typing import Iterator, Tuple
 import os
 from pathlib import Path
@@ -10,18 +9,17 @@ import lib
 rng = np.random.default_rng()
 directory = Path(f"{os.path.dirname(__file__)}")
 tests_path = directory / Path("tests")
-test_groups = 9
-group_size = 10
+group_size = 20
 matrix_size = 3
-eigenvalues_ratios = [1, 2, 5, 10, 100, 1000, 10000, 100000, 1000000]
-tested_params = [(1, 0), (2, 1), (3, 2), (3, 3), (4, 5), (4, 6), (4, 7)]
-tested_algorithms = ['QUBO_stable', 'QUBO_perturb']
+eigenvalues_ratios = [1, 2, 5, 10, 15, 20, 25, 30, 40, 50, 60, 70, 80, 90, 100]
+tested_params = [(i, int(2 ** i * .45)) for i in range(3, 16)]
 
 
-def generate_equations_result_eigenvalues(res_bound, eigenvalues_ratio, size: int, n: int) -> Iterator[Tuple[np.ndarray, np.ndarray, np.ndarray]]:
+def generate_equations_result_eigenvalues(res_bound, eigenvalues_ratio, size: int, n: int) -> Iterator[
+    Tuple[np.ndarray, np.ndarray, np.ndarray]]:
     for i in range(n):
         res = rng.uniform(-res_bound, res_bound, size)
-        eigenvalues = np.exp(rng.uniform(0, np.log(eigenvalues_ratio), size))
+        eigenvalues = np.hstack(([1, eigenvalues_ratio], rng.uniform(1, eigenvalues_ratio, size - 2)))
         a = ortho_group.rvs(size)
         A = a.T @ np.diag(eigenvalues) @ a
         b = lib.substitute_shift(A, np.zeros(size), -res)
@@ -35,35 +33,41 @@ def generate_tests() -> Iterator[Tuple[np.ndarray, np.ndarray, np.ndarray]]:
             yield e
 
 
-def solve_tests(tests: [Tuple[np.ndarray, np.ndarray, np.ndarray]], prec: int, neighbors: int) -> pd.DataFrame:
-    df = pd.DataFrame()
-    solutions = [[res, lib.solve(A, b, prec=prec, neighbors=neighbors),
-                  lib.solve(A, b, prec=prec, neighbors=neighbors, perturb=True)] for A, b, res in tests]
-    df[[f'{name}_solution' for name in ['reference'] + tested_algorithms]] = solutions
-    for name in tested_algorithms:
-        df[f'{name}_diff'] = np.vectorize(np.linalg.norm)(df['reference_solution'] - df[f'{name}_solution'])
-    return df
+def solve_tests(tests: [Tuple[np.ndarray, np.ndarray, np.ndarray]], prec: int, neighbors: int) -> np.ndarray:
+    ref, sol = map(lambda x: x.reshape(-1, group_size, matrix_size),
+                   map(np.array, zip(*[[res, lib.solve(A, b, prec=prec, neighbors=neighbors)] for A, b, res in tests])))
+    res = (np.count_nonzero(np.apply_along_axis(np.linalg.norm, 2, ref - sol) < 1, axis=1) >= group_size / 2)
+    return res
 
 
-def plot(data: pd.DataFrame, path: Path) -> None:
-    fig, axs = plt.subplots(3, 3, figsize=(10, 10))
-    fig.tight_layout(pad=2.5)
-    for i in range(test_groups):
-        axs[i // 3, i % 3].set_yscale('log')
-        axs[i // 3, i % 3].scatter(range(group_size), data[i * group_size:(i + 1) * group_size])
-        axs[i // 3, i % 3].set_title(f'eigenvalues ratio: {eigenvalues_ratios[i]}')
+def plot(total_solved: np.ndarray, max_group: np.ndarray) -> None:
     if not os.path.exists(tests_path):
         os.makedirs(tests_path)
-    plt.savefig(tests_path / path)
+    plt.title("Total algorithms to solve test case")
+    plt.xticks(np.arange(len(eigenvalues_ratios)), eigenvalues_ratios)
+    plt.yticks(np.arange(-1, len(tested_params) + 1))
+    plt.scatter(np.arange(len(eigenvalues_ratios)), total_solved)
+    plt.savefig(tests_path / Path("total"))
+    plt.clf()
+
+    plt.title("Hardest test case solved")
+    plt.xticks(np.arange(len(tested_params)), list(map(lambda x: f"{(2 * x[1] + 1)}/{(2 ** x[0])}", tested_params)))
+    plt.yticks(np.arange(len(eigenvalues_ratios)), eigenvalues_ratios)
+    plt.scatter(np.arange(len(tested_params)), max_group)
+    plt.savefig(tests_path / Path("hardest"))
     plt.clf()
 
 
 def test_all() -> None:
-    global tests_path
     tests = list(generate_tests())
-    for prec, neighbors in tested_params:
+    total_solved = np.zeros(len(eigenvalues_ratios))
+    max_group = np.zeros(len(tested_params))
+    for i, (prec, neighbors) in enumerate(tested_params):
         print(prec, neighbors)
-        tests_path = directory / Path(f"tests_{2 * neighbors + 1}_{2 ** prec}")
-        df = solve_tests(tests, prec, neighbors)
-        for name in tested_algorithms:
-            plot(df[f'{name}_diff'], Path(f'{name}_diff.png'))
+        res = solve_tests(tests, prec, neighbors)
+        total_solved += res
+        if np.all(res == 0):
+            max_group[i] = -1
+        else:
+            max_group[i] = np.max(np.nonzero(res))
+    plot(total_solved, max_group)
